71aa75c9826e080d9bf2152471a7fab9c2be8ac3
dylansawicki15 <dylansawicki15@gmail.com>
2025-11-05 21:52:40 +0100

    all changes needed for github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,7 +1,7 @@
 from extract_title import extract_title
 from markdown_to_html_node import markdown_to_html_node
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as file:
         markdown = file.read()
@@ -9,5 +9,8 @@ def generate_page(from_path, template_path, dest_path):
         template = file.read()
     title = extract_title(markdown)
     content = markdown_to_html_node(markdown).to_html()
+    html = template.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    html = html.replace('href="/', f'href="{basepath}')
+    html = html.replace('src="/', f'src="{basepath}')
     with open(dest_path, "w") as file:
-        file.write(template.replace("{{ Title }}", title).replace("{{ Content }}", content))
+        file.write(html)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+import sys
 from copy_directory_to_directory import copy_directory_to_directory
 from generate_pages_recursive import generate_pages_recursive
 
 if __name__ == "__main__":
-    copy_directory_to_directory("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    copy_directory_to_directory("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
